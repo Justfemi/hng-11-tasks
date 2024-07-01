d456734f943910bdf1bbb3efc7ce336cc17078ac
Justfemi <ajibade.olufemi@yahoo.com>
2024-07-01 06:31:41 +0100

    ch:wrote function for UTC date and time

--- task-stage1/script.js
+++ task-stage1/script.js
@@ -17,10 +17,6 @@ document.addEventListener('scroll', () => {
   hamburger.innerHTML = '<i class="bx bx-menu"></i>';
 });
 
-// navLinks[0].addEventListener('click', () => {
-//   nav.classList.remove('show');
-// })
-
 navLinks.forEach( function(link) {
   link.addEventListener('click', () => {
     navLinks.forEach(function(navLink) {
@@ -30,4 +26,30 @@ navLinks.forEach( function(link) {
     nav.classList.remove('show');
     hamburger.innerHTML = '<i class="bx bx-menu"></i>';
   })
-})
+});
+
+// Get current UTC time
+function updateUTCTime() {
+  const utcTimeElement = document.getElementById('utc-time');
+  const utcDayElement = document.getElementById('utc-day');
+  const now = new Date();
+
+  const hours = now.getUTCHours().toString().padStart(2, '0');
+  const minutes = now.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = now.getUTCSeconds().toString().padStart(2, '0');
+  
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+  
+  utcTimeElement.textContent = `${formattedTime}`;
+
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const dayOfWeek = daysOfWeek[now.getUTCDay()];
+
+  utcDayElement.textContent = `${dayOfWeek}`;
+}
+
+setInterval(updateUTCTime, 1000);
+
+updateUTCTime();
+
